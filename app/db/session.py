1b9cf8f833ddd9aeec6c3e1c54c6df49a6a068fd
Abhishek Pundir <abhishekatm1@gmail.com>
2026-01-27 16:33:15 +0530

    changes

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./trustllm.db"
+DATABASE_URL = "psql 'postgresql://neondb_owner:npg_ZOSU4ACW3xNT@ep-summer-dream-ah9akyph-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
